200c5c7f0770ebefa7135850b7c278bb696d7001
Dave <davepeck@gmail.com>
2024-10-14 23:19:34 +0000

    Minor clean up to match PEP 750.

--- pep/logging.py
+++ pep/logging.py
@@ -30,23 +30,23 @@ class TemplateMessageMaker:
 
 class TemplateMessage:
     def __init__(self, template: Template, encoder: Encoder | None = None) -> None:
-        self._template = template
+        self.template = template
         self.encoder = encoder or JSONEncoder()
 
     @property
     def message(self) -> str:
-        return f(self._template)
+        return f(self.template)
 
     @property
-    def values(self) -> dict[str, Any]:
+    def values(self) -> Mapping[str, Any]:
         return {
             arg.expr: arg.value
-            for arg in self._template.args
+            for arg in self.template.args
             if isinstance(arg, Interpolation)
         }
 
     @property
-    def data(self) -> dict[str, Any]:
+    def data(self) -> Mapping[str, Any]:
         return {"message": self.message, "values": self.values}
 
     def __str__(self) -> str:
@@ -82,7 +82,7 @@ class MessageFormatter(TemplateFormatterBase):
     def message(self, template: Template) -> str:
         return f(template)
 
-    def format(self, record: Any) -> str:
+    def format(self, record: LogRecord) -> str:
         msg = record.msg
         if not isinstance(msg, Template):
             return super().format(record)
