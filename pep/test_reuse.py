13ce5f87b78ddb26d5d4132ecca6bda0dd74671b
Dave <davepeck@gmail.com>
2024-10-21 22:46:45 +0000

    Minor cleanup

--- pep/test_reuse.py
+++ pep/test_reuse.py
@@ -6,4 +6,7 @@ def test_formatter():
     template: Template = t"This is a {'thing'} with value {'value':.2f}"
     formatter = Formatter(template)
     assert formatter.format(thing="pi", value=3.14159) == "This is a pi with value 3.14"
-    assert formatter.format(thing="e", value=2.71828) == "This is a e with value 2.72"
+    assert (
+        formatter.format(thing="cookie", value=42)
+        == "This is a cookie with value 42.00"
+    )
