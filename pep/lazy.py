9c16b747f679ac055cb1d47a1a0f2046916f5b03
Dave <davepeck@gmail.com>
2024-10-22 18:07:14 +0000

    Clarify the exact intent of format_some()

--- pep/lazy.py
+++ pep/lazy.py
@@ -6,18 +6,25 @@ from .fstring import convert
 
 def format_some(selector: str, template: Template, ignored: str = "***") -> str:
     """
-    Given a template where all interpolations are callables, invoke the
-    callables only when the format_spec is `:selector`.
+    Render a template to string in a stylized way.
+
+    Render only those interpolations whose `format_spec` matches `selector`.
+
+    In addition, if an interpolation is a callable, only invoke it when the
+    `format_spec` matches `selector`. This allows us to defer computation
+    until we know it's needed; for instance, in a logging pipeline where
+    some expressions are expensive to compute and later prove to be
+    unnecessary.
     """
     parts = []
     for t_arg in template.args:
         if isinstance(t_arg, str):
             parts.append(t_arg)
         else:
-            if not callable(t_arg.value):
-                raise ValueError(f"Non-callable interpolation: {t_arg.value}")
             if t_arg.format_spec == selector:
                 value = t_arg.value()
+                if callable(value):
+                    value = value()
                 value = convert(value, t_arg.conv)
             else:
                 value = ignored
